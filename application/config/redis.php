5729d6490949e6238eb708a94d2a64b64808b116
Zhanglong <nice5good@126.com>
2018-01-25 19:58:25 +0800

    配置redis

---
+++ application/config/redis.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Redis 配置
+ */
+$config['socket_type'] = 'tcp'; //`tcp` or `unix`
+$config['host'] = '127.0.0.1';
+$config['password'] = NULL;
+$config['port'] = 6379;
+$config['timeout'] = 0;
